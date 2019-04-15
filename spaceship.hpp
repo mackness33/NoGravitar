22f654cd402d8485f5b5ee2bace573f2a98a613d
mackness33 <davidantonhy962@gmail.com>
2019-04-15 17:04:03 +0200

    Primo commit di Micol

--- spaceship.hpp
+++ spaceship.hpp
@@ -3,14 +3,25 @@
 
 class spaceship : public sf::Text{
 private:
+  float spatial_Versor;
+  float rotation_Versor;
   sf::Transformable myTransform;
 
 public:
 
   //CONSTRUCTORS
+  spaceship(sf::Text* t, sf::Font* f, float sv, float rv) : sf::Text(*t){
+    this->setFont(*f);
+    this->setString("Spacestrip");
+    spatial_Versor = sv;
+    rotation_Versor = rv;
+  }
+
   spaceship(sf::Text* t, sf::Font* f) : sf::Text(*t){
     this->setFont(*f);
     this->setString("Spacestrip");
+    spatial_Versor = 0;
+    rotation_Versor = 0;
   }
 
   spaceship(sf::Font* f) : sf::Text(){
@@ -27,68 +38,33 @@ public:
     // set the text style
     this->setStyle(sf::Text::Bold | sf::Text::Underlined);
 
+    spatial_Versor = 0;
+    rotation_Versor = 0;
   }
 
   //SETS
 
   //METHODS
-  void SetPosition(float x, float y){
-    myTransform.setPosition(x, y);
-  }
-
-  void Draw(sf::RenderTarget& target) const{
-    target.draw(*this, myTransform.getTransform());
-  }
-
-  void Move(float x, float y){
-    myTransform.move(x, y);
-  }
-
-  void Rotate(float g){
-    myTransform.rotate(g);
-  }
-
-  const sf::Vector2f& GetPosition() const{
-    return myTransform.getPosition();
-  }
-
   void movement(sf::Keyboard::Key* k){
-    float shift = 10;
-
     switch (*k) {
       case sf::Keyboard::Left : {
         this->rotate(1);
       };break;           //LEFT
+
       case sf::Keyboard::Right : {
         this->rotate(-1);
       };break;           //RIGHT
-      case sf::Keyboard::Up : { this->move(cos(this->getRotation() * PI / 180.0) * (shift), sin(this->getRotation() * PI / 180.0) * (shift)); };break;             //UP
-      case sf::Keyboard::Down : { this->move(cos(this->getRotation() * PI / 180.0) * -shift, sin(this->getRotation() * PI / 180.0) * -shift); };break;            //DOWN
+
+      case sf::Keyboard::Up : {
+        this->move(cos(this->getRotation() * PI / 180.0) * (10), sin(this->getRotation() * PI / 180.0) * (10));
+      };break;             //UP
+
+      case sf::Keyboard::Down : {
+        this->move(cos(this->getRotation() * PI / 180.0) * -10, sin(this->getRotation() * PI / 180.0) * -10);
+      };break;            //DOWN
+
       default: std::cout << "Not a movementCommand" << std::endl;
     }
   }
-
-  /*sf::Keyboard::Key smooth_movement(bool* t, bool* r){
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-      *t  = true;
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        this->movement(sf::Keyboard::Up);            //UP
-      else
-        this->movement(sf::Keyboard::Down);            //DOWN
-    }
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-      *r  = true;
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        this->movement(sf::Keyboard::Left);            //UP
-      else
-        this->movement(sf::Keyboard::Right);            //DOWN
-    }
-
-    return sf::Keyboard::Unknown;
-  }*/
-
 
 };

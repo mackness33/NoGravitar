3b4fb95369021eaa58746001d81c74c81958c998
micolll <micol.dromedari@gmail.com>
2019-04-16 19:25:08 +0200

    prova del pianeta

---
+++ planet.hpp
@@ -0,0 +1,22 @@
+
+
+class planet : public sf::Vertex{
+
+public:
+  planet(sf::Vertex* s) : sf::Vertex(*s){
+
+  }
+  void build (){
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow Window(sf::VideoMode(desktop.height, desktop.width, desktop.bitsPerPixel), "NoGravitar");
+
+
+
+
+
+      sf::Vertex line[] =
+  {
+      sf::Vertex(sf::Vector2f(0.f, ((desktop.height)/2.f))),
+      sf::Vertex(sf::Vector2f((desktop.width)/1.f, (desktop.height)/2.f))
+
+  };
